e3ea37fc3cd0df25b37587478ea944eac0b7d36f
UupDownPark <geakura123@gmail.com>
2024-03-25 22:38:35 +0900

    start

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
-import Header from "../components/Header";
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
 
 function Home() {
   return (
     <>
       <Header />
       <div>북 스토어</div>
+      <Footer />
     </>
   );
 }
